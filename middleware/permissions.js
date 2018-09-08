d62fade437c91a777bbbcbae3cd2243f7921c4c2
Jeff McAffer <jmcaffer@microsoft.com>
2018-09-08 13:24:32 -0400

    make permissions check more robust

--- middleware/permissions.js
+++ middleware/permissions.js
@@ -1,23 +1,22 @@
 // Copyright (c) Amazon.com, Inc. or its affiliates and others. Licensed under the MIT license.
 // SPDX-License-Identifier: MIT
 
+const { get, intersection } = require('lodash')
 /**
- * Middleware that checks for team membership.
+ * Middleware that checks for permissions for this request based on team membership.
  *
- * Usage: `app.get('/some/route', teamCheck('harvesters'), (req, res) => ...)`
+ * Usage: `app.get('/some/route', permissionCheck('harvesters'), (req, res) => ...)`
  */
 function permissionCheck(permission) {
-  return (req, res, next) => {
-    const userTeams = req.app.locals.user.github.teams
-    const requiredTeams = req.app.locals.config.auth.github.permissions[permission] // whew!
-    const intersection = requiredTeams.filter(t => userTeams.includes(t))
-    if (requiredTeams.length === 0 || intersection.length > 0) {
-      next()
-    } else {
-      const err = new Error(`No permission to '${permission}' (needs team membership)`)
-      err.status = 401
-      next(err)
-    }
+  // eslint-disable-next-line no-unused-vars
+  return (request, response, next) => {
+    const requiredTeams = get(request, `app.locals.config.auth.github.permissions[${permission}]`) // whew!
+    if (!requiredTeams || requiredTeams.length === 0) return next()
+    const userTeams = get(request, 'app.locals.user.github.teams')
+    if (intersection(requiredTeams, userTeams).length > 0) return next()
+    const err = new Error(`No permission to '${permission}' (needs team membership)`)
+    err.status = 401
+    next(err)
   }
 }
 
