c10deb3fd12d98d5cc31e9a7d4505e485f96a34d
moranthomas <tom_moran@hotmail.com>
2018-10-29 09:37:04 +0000

    adding PR feedback into azure search request promise

--- providers/search/azureSearch.js
+++ providers/search/azureSearch.js
@@ -41,21 +41,35 @@ class AzureSearch extends AbstractSearch {
    */
   store(definitions) {
     const entries = this._getEntries(Array.isArray(definitions) ? definitions : [definitions])
-    return requestPromise({
+
+    const cases = {
+      200: '200 OK -  Success',
+      207: '207 - At least one item was not successfully indexed',
+      400: '400 - Bad Request',
+      401: '401 - Unauthorized',
+      404: '404 - Not Found',
+      429: '429 - You have exceeded your quota on the number of documents per index.',
+      503: '503 -The system is under heavy load and your request cannot be processed at this time.'
+    }
+    const options = {
       method: 'POST',
       url: this._buildUrl(`indexes/${coordinatesIndexName}/docs/index`),
       headers: this._getHeaders(),
       body: { value: entries },
       withCredentials: false,
       resolveWithFullResponse: true
-    })
-    .then(function (res) {
-        console.log("POST returned with status %d", handleResponseCodes(res.statusCode));
-    })
-    .catch(function (err) {
-        console.log("POST failed with status %d", err.statusCode);
-        console.log(err)
-    });
+    }
+
+    try{
+        const result = await requestPromise(options, function(err, res, body) {
+          if(cases[res.statusCode] === '200') {
+            return cases[res.statusCode]
+          }
+        })
+    } catch (err) {
+        return cases[err.statusCode]
+    }
+
   }
 
   _getEntries(definitions) {
@@ -81,21 +95,36 @@ class AzureSearch extends AbstractSearch {
    * @param {EntityCoordinates} coordinates - the coordinates of the definition to delete
    */
   delete(coordinates) {
-    return requestPromise({
+
+    const cases = {
+      200: '200 OK -  Success',
+      207: '207 - At least one item was not successfully indexed',
+      400: '400 - Bad Request',
+      401: '401 - Unauthorized',
+      404: '404 - Not Found',
+      429: '429 - You have exceeded your quota on the number of documents per index.',
+      503: '503 -The system is under heavy load and your request cannot be processed at this time.'
+    }
+    const options = {
       method: 'POST',
       url: this._buildUrl(`indexes/${coordinatesIndexName}/docs/index`),
       headers: this._getHeaders(),
       body: { value: [{ '@search.action': 'delete', key: this._getKey(coordinates) }] },
       withCredentials: false,
       resolveWithFullResponse: true
-    })
-    .then(function (res) {
-        console.log("POST returned with status %d", handleResponseCodes(res.statusCode));
-    })
-    .catch(function (err) {
-        console.log("POST failed with status %d", err.statusCode);
-        console.log(err)
-    });
+    }
+
+    try{
+        const result = await requestPromise(options, function(err, res, body) {
+          if(cases[res.statusCode] === '200') {
+            return cases[res.statusCode]
+          }
+        })
+    } catch (err) {
+        return cases[err.statusCode]
+    }
+
+
   }
 
   _getKey(coordinates) {
